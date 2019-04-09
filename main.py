1511d70ee41739653c27a8e5c28c720d52ea37b2
Kennylele <tobias@schochwebdesign.de>
2019-04-09 14:00:57 +0200

    now print()

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ vec = np.zeros((21, 3))
 rang = ["10", "13", "16", "19", "22", "25", "28", "31", "34", "37", "40", "43", "46", "49", "52", "55", "58", "61",
         "64", "67", "70"]
 
-between = range[1001, 2000, 1]
-
 for x in rang:
-    for y in between:
-        ten += 1
-        data = np.genfromtxt('data/' + str(rang[ten]) + '.csv', delimiter=",")
-        count += 1
-        average = average
+    ten += 1
+    data = np.genfromtxt('data/' + str(rang[ten]) + '.csv', delimiter=",", skip_header=1000, skip_footer=499)
 
-    print("Der Durchschnitt bei " + str(rang[ten]) + "cm Entfernung beträgt: " + str(average) + " cm")
+    for line in data:
+        count += 1
+        average = average + line
+
+        average = average / count
+
+    print("Der Durchschnitt bei " + str(rang[ten]) + "cm Entfernung beträgt: " + str(
+            average) + " cm" + " und hat eine Standartabweichung von " + str(np.std))
 
     vec[ten, 0] = str(rang[ten])
-    #vec[ten, 1] = np.mean(data[1001, 2000])
-    #vec[ten, 2] = np.std(data[1001, 2000])
-
+    # vec[ten, 1] = np.mean(data[1001, 2000])
+    # vec[ten, 2] = np.std(data[1001, 2000])
