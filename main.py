9751764de38046907e9f346341626f076413cfb7
Luca Stratmann <luca.stratmannmx-topmail.de>
2019-04-09 15:56:48 +0200

    Finished Task 1

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ for x in rang:
     vec[ten, 1] = np.nanmean(data)
     vec[ten, 2] = np.nanstd(data)
 
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
+plt.plot([10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70], [vec[0, 1], vec[1, 1], vec[2, 1], vec[3, 1], vec[4, 1], vec[5, 1], vec[6, 1], vec[7, 1], vec[8, 1], vec[9, 1], vec[10, 1], vec[11, 1], vec[12, 1], vec[13, 1], vec[14, 1], vec[15, 1], vec[16, 1], vec[17, 1], vec[18, 1], vec[19, 1], vec[20, 1]],  'b')
+plt.ylabel('Durchschnitt')
+plt.xlabel('cm')
+plt.axis([0, 70, 0, 1])
+plt.show()
+
+plt.plot([10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70], [vec[0, 2], vec[1, 2], vec[2, 2], vec[3, 2], vec[4, 2], vec[5, 2], vec[6, 2], vec[7, 2], vec[8, 2], vec[9, 2], vec[10, 2], vec[11, 2], vec[12, 2], vec[13, 2], vec[14, 2], vec[15, 2], vec[16, 2], vec[17, 2], vec[18, 2], vec[19, 2], vec[20, 2]],  'r')
+plt.ylabel('Standardabweichung')
+plt.xlabel('cm')
+plt.axis([0, 70, 0, 1])
 plt.show()
