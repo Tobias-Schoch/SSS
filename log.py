34f627195b32e09fa8863161822aafd37eb7315f
Kennylele <tobias@schochwebdesign.de>
2019-04-10 11:52:15 +0200

    starting second task

--- log.py
+++ log.py
@@ -3,67 +3,23 @@ import matplotlib.pyplot as plt
 
 ten = -1
 data = 0
-data2 = 0
-data3 = 0
-
-vec = np.zeros((21, 3))
-vol = np.zeros((21, 3))
+vec2 = np.zeros((21, 3))
 
 rang = ["10", "13", "16", "19", "22", "25", "28", "31", "34", "37", "40", "43", "46", "49", "52", "55", "58", "61",
         "64", "67", "70"]
 
 for x in rang:
     ten += 1
-    data = np.genfromtxt('data/' + str(rang[ten]) + '.csv', delimiter=",", skip_header=1000, skip_footer=499)
+    data = np.genfromtxt('data/' + str(rang[ten]) + '.csv', delimiter=",", skip_header=1000, skip_footer=499, usecols=(4))
 
-    print("Durchschnitt " + str(rang[ten]) + "cm:" + str(np.nanmean(data)))
-    print("Standartabweichung " + str(rang[ten]) + "cm:" + str(np.nanstd(data)) + "\n")
+    print("Durchschnitt " + str(rang[ten]) + "cm:" + str(np.mean(data)))
 
-    vec[ten, 0] = rang[ten]
-    vec[ten, 1] = np.nanmean(data)
-    vec[ten, 2] = np.nanstd(data)
+    vec2[ten, 0] = rang[ten]
+    vec2[ten, 1] = np.log(np.mean(data))
+    vec2[ten, 2] = np.log(np.std(data))
 
-    data2 = np.genfromtxt('data/' + str(rang[ten]) + '.csv', delimiter=",", skip_header=1000, skip_footer=499)
-    data3 = np.genfromtxt('data/' + str(rang[ten]) + '.csv', delimiter=",", skip_header=1000, skip_footer=499)
-
-    vol[ten, 0] = rang[ten]
-    vol[ten, 1] = np.log(data)
-
-
-
-
-
-ten = -1
-
-plt.axis([0,80,0,0.8])
-plt.xlabel('Entfernung')
-plt.ylabel('Durchschnitt')
-plt.plot([vec[0, 0], vec[1, 0], vec[2, 0],vec[3, 0], vec[4, 0],
-          vec[5, 0],vec[6, 0],vec[7, 0],vec[8, 0],vec[9, 0],
-          vec[10, 0],vec[11, 0],vec[12, 0],vec[13, 0],vec[14, 0],
-          vec[15, 0],vec[16, 0],vec[17, 0],vec[18, 0],vec[19, 0],
-          vec[20, 0]],
-         [vec[0, 1],vec[1, 1],vec[2, 1],vec[3, 1],vec[4, 1],
-          vec[5, 1],vec[6, 1],vec[7, 1],vec[8, 1],vec[9, 1],
-          vec[10, 1],vec[11, 1],vec[12, 1],vec[13, 1],
-          vec[14, 1],vec[15, 1],vec[16, 1],vec[17, 1],
-          vec[18, 1],vec[19, 1],vec[20, 1]], 'r')
+plt.plot([vec2[0, 0], vec2[1, 0], vec2[2, 0], vec2[3, 0], vec2[4, 0], vec2[5, 0], vec2[6, 0], vec2[7, 0], vec2[8, 0], vec2[9, 0], vec2[10, 0], vec2[11, 0], vec2[12, 0], vec2[13, 0], vec2[14, 0], vec2[15, 0], vec2[16, 0], vec2[17, 0], vec2[18, 0], vec2[19, 0], vec2[20, 0]], [vec2[0, 1], vec2[1, 1], vec2[2, 1], vec2[3, 1], vec2[4, 1], vec2[5, 1], vec2[6, 1], vec2[7, 1], vec2[8, 1], vec2[9, 1], vec2[10, 1], vec2[11, 1], vec2[12, 1], vec2[13, 1], vec2[14, 1], vec2[15, 1], vec2[16, 1], vec2[17, 1], vec2[18, 1], vec2[19, 1], vec2[20, 1]],  'b')
+plt.ylabel('Durchschnitt von Log')
+plt.xlabel('cm')
+plt.axis([0, 70, -1.2, 1])
 plt.show()
-
-
-plt.axis([0,80,0,0.8])
-plt.xlabel('Entfernung')
-plt.ylabel('Standartabweichung')
-plt.plot([vec[0, 0], vec[1, 0], vec[2, 0],vec[3, 0], vec[4, 0],
-          vec[5, 0],vec[6, 0],vec[7, 0],vec[8, 0],vec[9, 0],
-          vec[10, 0],vec[11, 0],vec[12, 0],vec[13, 0],vec[14, 0],
-          vec[15, 0],vec[16, 0],vec[17, 0],vec[18, 0],vec[19, 0],
-          vec[20, 0]],
-         [vec[0, 2],vec[1, 2],vec[2, 2],vec[3, 2],vec[4, 2],
-          vec[5, 2],vec[6, 2],vec[7, 2],vec[8, 2],vec[9, 2],
-          vec[10, 2],vec[11, 2],vec[12, 2],vec[13, 2],
-          vec[14, 2],vec[15, 2],vec[16, 2],vec[17, 2],
-          vec[18, 2],vec[19, 2],vec[20, 2]], 'b')
-plt.show()
-
-
