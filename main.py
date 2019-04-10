6d94705500df0e327925e4ecb251fa275013cb38
Luca Stratmann <luca.stratmannmx-topmail.de>
2019-04-10 11:51:48 +0200

    Splitting Tasks

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 ten = -1
 data = 0
 vec = np.zeros((21, 3))
-vec2 = np.zeros((21, 3))
+
 
 rang = ["10", "13", "16", "19", "22", "25", "28", "31", "34", "37", "40", "43", "46", "49", "52", "55", "58", "61",
         "64", "67", "70"]
@@ -18,11 +18,9 @@ for x in rang:
     print("Standartabweichung " + str(rang[ten]) + "cm:" + str(np.std(data)) + "\n")
 
     vec[ten, 0] = rang[ten]
-    vec[ten, 1] = np.nanmean(data)
-    vec[ten, 2] = np.nanstd(data)
-    vec2[ten, 0] = rang[ten]
-    vec2[ten, 1] = np.log(np.mean(data))
-    vec2[ten, 2] = np.log(np.std(data))
+    vec[ten, 1] = np.mean(data)
+    vec[ten, 2] = np.std(data)
+
 
 
 
@@ -39,10 +37,5 @@ plt.xlabel('cm')
 plt.axis([0, 70, 0, 0.12])
 plt.show()
 
-plt.plot([vec2[0, 0], vec2[1, 0], vec2[2, 0], vec2[3, 0], vec2[4, 0], vec2[5, 0], vec2[6, 0], vec2[7, 0], vec2[8, 0], vec2[9, 0], vec2[10, 0], vec2[11, 0], vec2[12, 0], vec2[13, 0], vec2[14, 0], vec2[15, 0], vec2[16, 0], vec2[17, 0], vec2[18, 0], vec2[19, 0], vec2[20, 0]], [vec2[0, 1], vec2[1, 1], vec2[2, 1], vec2[3, 1], vec2[4, 1], vec2[5, 1], vec2[6, 1], vec2[7, 1], vec2[8, 1], vec2[9, 1], vec2[10, 1], vec2[11, 1], vec2[12, 1], vec2[13, 1], vec2[14, 1], vec2[15, 1], vec2[16, 1], vec2[17, 1], vec2[18, 1], vec2[19, 1], vec2[20, 1]],  'b')
-plt.ylabel('Durchschnitt von Log')
-plt.xlabel('cm')
-plt.axis([0, 70, -1.2, 1])
-plt.show()
 
 
