ddb0adbf174dce8eb4227b340e470ea8a7dcf0b4
Kennylele <tobias@schochwebdesign.de>
2019-04-12 18:16:12 +0200

    last format changes

--- Task3.py
+++ Task3.py
@@ -1,16 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
-import math
-
 
 data = 0
 data2 = 0
-vec1 = np.zeros((2, 3))
-korrektur_breit1 = 0
-korrektur_lang1 = 0
-korrektur_breit2 = 0
-korrektur_lang2 = 0
 faktor1 = 1
 faktor2 = 1.96
 
@@ -19,34 +10,34 @@ data = np.genfromtxt('data/DinA4Breit.csv', delimiter=",", skip_header=1000, ski
 data2 = np.genfromtxt('data/DinA4Lang.csv', delimiter=",", skip_header=1000, skip_footer=499,
                       usecols=(4))
 
+stde1 = np.std(data2) / np.sqrt(1000)
 
+a1 = np.log(29.7) / np.log(np.mean(data2))
+b1 = np.log(29.7) - (a1 * np.log(np.mean(data2)))
+z1 = (np.exp(b1) * a1 * np.mean(data2) ** (a1 - 1))
 
-vec1[0, 0] = 21
-vec1[0, 1] = np.mean(data)
-vec1[0, 2] = np.std(data)
+korrektur_lang = (np.mean(data2) + faktor2 * stde1) - np.mean(data2)
+deltaya65 = z1 * stde1
+deltaya96 = z1 * korrektur_lang
 
-vec1[1, 0] = 29.7
-vec1[1, 1] = np.mean(data2)
-vec1[1, 2] = np.std(data2)
+y = np.exp(b1) * np.power(np.mean(data2), a1)
 
-korrektur_breit1 = 0.861 + faktor1 * vec1[0, 2]
-korrektur_lang1 = 0.662 + faktor1 * vec1[1, 2]
-korrektur_breit2 = 0.861 + faktor2 * vec1[0, 2]
-korrektur_lang2 = 0.662 + faktor2 * vec1[1, 2]
-
-x = math.exp(np.log(vec1[1, 1]))
-x1 = np.array([vec1[0, 1]])
-x2 = np.array([vec1[0, 1]])
-
-gradient, intercept, r_value, p_value, std_err = stats.linregress(x1, x2)
-z1 = (math.exp(intercept)*gradient*x**(gradient-1)) * korrektur_breit1
-z2 = math.exp(intercept)*gradient*x**(gradient-1) * korrektur_breit2
+print(y)
+print("29.7 cm", deltaya65, "cm 68%")
+print("29.7 cm", deltaya96, "cm 95%")
 
 
 
-print("68,26%: " + str(vec1[0, 1]))
-print("68,26%: " + str(korrektur_lang1))
-print("95%: " + str(vec1[0, 1]))
-print("95%: " + str(korrektur_lang2))
-print(str(z1))
-print(str(z2))
+stde2 = np.std(data) / np.sqrt(1000)
+a2 = np.log(21) / np.log(np.mean(data))
+b2 = np.log(21) - (a2 * np.log(np.mean(data)))
+z2 = (np.exp(b2) * a2 * np.mean(data) ** (a2 - 1))
+
+korrektur_breit = (np.mean(data) + faktor2 * stde2) - np.mean(data)
+deltayb65 = z2 * stde2
+deltayb96 = z2 * korrektur_breit
+
+print("21 cm", deltayb65, "cm 68%")
+print("21 cm", deltayb96, "cm 95%")
+print("623,7 cm^2", (deltaya65 + deltayb65), "cm bei 68%")
+print("623,7 cm^2", (deltaya96 + deltayb96), "cm bei 95%")
