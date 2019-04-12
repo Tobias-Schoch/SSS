fe4deec91b158c48b801628a240f43dff5306c44
Luca Stratmann <luca.stratmannmx-topmail.de>
2019-04-12 14:29:49 +0200

    more testing

--- Task3.py
+++ Task3.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import math
 
 
 data = 0
@@ -16,8 +19,10 @@ data = np.genfromtxt('data/DinA4Breit.csv', delimiter=",", skip_header=1000, ski
 data2 = np.genfromtxt('data/DinA4Lang.csv', delimiter=",", skip_header=1000, skip_footer=499,
                       usecols=(4))
 
+
+
 vec1[0, 0] = 21
-vec1[0, 1] = np.mean(data)
+h= vec1[0, 1] = np.mean(data)
 vec1[0, 2] = np.std(data)
 
 vec1[1, 0] = 29.7
@@ -29,7 +34,15 @@ korrektur_lang1 = 0.662 + faktor1 * vec1[1, 2]
 korrektur_breit2 = 0.861 + faktor2 * vec1[0, 2]
 korrektur_lang2 = 0.662 + faktor2 * vec1[1, 2]
 
+x = korrektur_breit1
+y = korrektur_breit2
+
+gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+z = math.log(intercept)*gradient*h**(gradient-1)
+
+
 print("68,26%: " + str(korrektur_breit1))
 print("68,26%: " + str(korrektur_lang1))
 print("95%: " + str(korrektur_breit2))
 print("95%: " + str(korrektur_lang2))
+print(str(z))
