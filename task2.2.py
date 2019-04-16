9921653841c5dd80c1b0f878812652fc8c2ffd59
Kennylele <tobias@schochwebdesign.de>
2019-04-16 23:47:58 +0200

    white - black = whitepicture

--- task2.2.py
+++ task2.2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-# ------------------------------ Aufgabe3.1 ------------------------------ #
+# ------------------------------ Aufgabe2.1 ------------------------------ #
 
 vec = np.zeros((10, 3079000))
 anzahl = 0
@@ -17,19 +17,20 @@ vec7 = np.zeros((480, 640))
 vec8 = np.zeros((480, 640))
 vec9 = np.zeros((480, 640))
 vec10 = np.zeros((480, 640))
+
 average = np.zeros((480, 640))
 
-whitepic = ["white1", "white2", "white3", "white4", "white5", "white6", "white7", "white8", "white9", "white10"]
+blackpic = ["black1", "black2", "black3", "black4", "black5", "black6", "black7", "black8", "black9", "black10"]
 vector = [vec1, vec2, vec3, vec4, vec5, vec6, vec7, vec8, vec9, vec10]
 
 for x in range(0, 10):
-    whitepic[x] = cv2.imread('data/' + whitepic[x] + '.png')
-    whitepic[x] = cv2.cvtColor(whitepic[x], cv2.COLOR_BGR2GRAY)
+    blackpic[x] = cv2.imread('data/' + blackpic[x] + '.png')
+    blackpic[x] = cv2.cvtColor(blackpic[x], cv2.COLOR_BGR2GRAY)
 
     print("Datei: " + str(x + 1) + ".png erfolgreich")
     for y in range(0, 480):
         for z in range(0, 640):
-            b = whitepic[x][y, z]
+            b = blackpic[x][y, z]
             vector[x][y, z] = b
             anzahl += 1
     print("-----------------------------------")
@@ -42,12 +43,12 @@ for y in range(0, 480):
         mean = float(mean / 10)
         average[y, z] = mean
 
-image = cv2.imread("data/whiteaverage.png")
+image = cv2.imread("data/blackaverage.png")
 for x in range(0, 480):
     for y in range(0, 640):
         image[x, y] = average[x, y]
 
-cv2.imwrite("data/whiteaverage.png", image)
+cv2.imwrite("data/blackaverage.png", image)
 cv2.imshow('image', image)
 
 # Convert to YUV
@@ -57,7 +58,7 @@ image_contrast[:, :, 0] = cv2.equalizeHist(image_contrast[:, :, 0])
 # Convert to RGB
 image_contrast = cv2.cvtColor(image_contrast, cv2.COLOR_YUV2RGB)
 
-cv2.imwrite("data/contrastwhiteaverage1.png", image_contrast)
+cv2.imwrite("data/contrastblackaverage.png", image_contrast)
 cv2.imshow('image_contrast', image_contrast)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- task3.2.py
+++ task3.2.py
@@ -3,3 +3,36 @@ import cv2
 
 # ------------------------------ Aufgabe3.2 ------------------------------ #
 
+white_minus_black = np.zeros((480, 640))
+
+whiteavg = cv2.imread('data/whiteaverage.png')
+blackavg = cv2.imread('data/blackaverage.png')
+
+
+
+
+for y in range(0, 480):
+    for z in range(0, 640):
+        r1, g1, b1 = whiteavg[y, z]
+        r2, g2, b2 = blackavg[y, z]
+        white_minus_black[y, z] = int(r1) - int(r2)
+
+image = cv2.imread("data/whiteminusblack.png")
+for x in range(0, 480):
+    for y in range(0, 640):
+        image[x, y] = white_minus_black[x, y]
+
+cv2.imwrite("data/whiteminusblack.png", image)
+cv2.imshow('image', image)
+
+# Convert to YUV
+image_contrast = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+# Apply histogram equalization
+image_contrast[:, :, 0] = cv2.equalizeHist(image_contrast[:, :, 0])
+# Convert to RGB
+image_contrast = cv2.cvtColor(image_contrast, cv2.COLOR_YUV2RGB)
+
+cv2.imwrite("data/contrastwhiteaverage2.png", image_contrast)
+cv2.imshow('image_contrast', image_contrast)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
