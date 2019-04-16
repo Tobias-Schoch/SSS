d139af6d82746bc88b1519479e8816cff9ab8076
Kennylele <tobias@schochwebdesign.de>
2019-04-17 01:44:43 +0200

    normierung

--- task3.3.py
+++ task3.3.py
@@ -1,20 +1,33 @@
 import numpy as np
 import cv2
+import math
 
 # ------------------------------ Aufgabe2.2 ------------------------------ #
 
 korrigiertes_bild = np.zeros((480, 640))
+norm = 0
 
 grey = cv2.imread('data/Versuch1a.png')
 image = cv2.cvtColor(grey, cv2.COLOR_BGR2GRAY)
 blackavg = cv2.imread('data/blackaverage.png')
+whiteavg = cv2.imread('data/whiteaverage.png')
 
+b, g, r, a = cv2.mean(whiteavg)
+
+
+norm = b ** 2 + g ** 2 + r ** 2
+norm = math.sqrt(norm)
+b = b / norm
 
 for y in range(0, 480):
     for z in range(0, 640):
         r1, g1, b1 = grey[y, z]
         r2, g2, b2 = blackavg[y, z]
         korrigiertes_bild[y, z] = int(r1) - int(r2)
+        print("----------------------------------")
+        print(korrigiertes_bild[y, z])
+        korrigiertes_bild[y, z] *= b
+        print(korrigiertes_bild[y, z])
 
 image = cv2.imread("data/korrigiertes_bild2.png")
 for x in range(0, 480):
