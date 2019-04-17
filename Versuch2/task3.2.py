bba4121a444cbbecb8d28b1b2c2aad1c50afae6a
Kennylele <tobias@schochwebdesign.de>
2019-04-18 00:11:11 +0200

    adding comments

--- Versuch2/task3.2.py
+++ Versuch2/task3.2.py
@@ -3,32 +3,38 @@ import cv2
 
 # ------------------------------ Aufgabe3.2 ------------------------------ #
 
+# Vektor um die Werte der Subtraktion zu speichern
 white_minus_black = np.zeros((480, 640))
 
+# Weiß und Schwarz einlesen
 whiteavg = cv2.imread('data/whiteaverage.png')
 blackavg = cv2.imread('data/blackaverage.png')
 
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for y in range(0, 480):
     for z in range(0, 640):
+        # Grau-Werte berechnen
         r1, g1, b1 = whiteavg[y, z]
         r2, g2, b2 = blackavg[y, z]
+        # Weiß - Schwarz
         white_minus_black[y, z] = int(r1) - int(r2)
 
+# Bild einlesen als Grundlage
 image = cv2.imread("data/whiteminusblack.png")
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for x in range(0, 480):
     for y in range(0, 640):
+        # neue Pixel überschreiben von weiß - schwarz einfügen
         image[x, y] = white_minus_black[x, y]
 
+# Bild exportieren und anzeigen
 cv2.imwrite("data/whiteminusblack.png", image)
 cv2.imshow('image', image)
 
-# Convert to YUV
-image_contrast = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-# Apply histogram equalization
-image_contrast[:, :, 0] = cv2.equalizeHist(image_contrast[:, :, 0])
-# Convert to RGB
-image_contrast = cv2.cvtColor(image_contrast, cv2.COLOR_YUV2RGB)
+# Bild kontrastmaximiert darstellen
+image_contrast = cv2.equalizeHist(image)
 
+# Bild exportieren und anzeigen
 cv2.imwrite("data/contrastwhiteaverage2.png", image_contrast)
 cv2.imshow('image_contrast', image_contrast)
 cv2.waitKey(0)
