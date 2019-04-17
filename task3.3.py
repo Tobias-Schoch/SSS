7d3b3b3d765196bc2511d9917484db756153fe2c
Kennylele <tobias@schochwebdesign.de>
2019-04-17 16:15:13 +0200

    added highest and lowest colorvalue

--- task3.3.py
+++ task3.3.py
@@ -5,33 +5,28 @@ import math
 # ------------------------------ Aufgabe2.2 ------------------------------ #
 
 korrigiertes_bild = np.zeros((480, 640))
-norm = 0
+norm = np.zeros((480, 640))
 
 grey = cv2.imread('data/Versuch1a.png')
 image = cv2.cvtColor(grey, cv2.COLOR_BGR2GRAY)
 blackavg = cv2.imread('data/blackaverage.png')
-whiteavg = cv2.imread('data/whiteaverage.png')
-
-b, g, r, a = cv2.mean(whiteavg)
-
-
-norm = b ** 2 + g ** 2 + r ** 2
-norm = math.sqrt(norm)
-b = b / norm + g / norm + r / norm
-print(b)
+whiteminusblack = cv2.imread('data/whiteminusblack.png')
 
 for y in range(0, 480):
     for z in range(0, 640):
+        r, g, b = whiteminusblack[y, z] - np.mean(whiteminusblack)
         r1, g1, b1 = grey[y, z]
         r2, g2, b2 = blackavg[y, z]
         korrigiertes_bild[y, z] = int(r1) - int(r2)
-        korrigiertes_bild[y, z] /= b
+        korrigiertes_bild[y, z] /= r
+        print("pixelvalue:", korrigiertes_bild[y, z])
+        print("------------------------------")
 
 image = cv2.imread("data/korrigiertes_bild.png")
 for x in range(0, 480):
     for y in range(0, 640):
         image[x, y] = korrigiertes_bild[x, y]
-
+print("finish2")
 cv2.imwrite("data/korrigiertes_bild2.png", image)
 cv2.imshow('image', image)
 cv2.waitKey(0)
