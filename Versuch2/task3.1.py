4d129719864debe01ea3f4fa433961216131fdb7
Kennylele <tobias@schochwebdesign.de>
2019-04-18 00:08:31 +0200

    adding comments

--- Versuch2/task3.1.py
+++ Versuch2/task3.1.py
@@ -3,9 +3,7 @@ import cv2
 
 # ------------------------------ Aufgabe3.1 ------------------------------ #
 
-anzahl = 0
-mean = 0
-
+# Vectoren für die 10 Weißbildern
 vec1 = np.zeros((480, 640))
 vec2 = np.zeros((480, 640))
 vec3 = np.zeros((480, 640))
@@ -16,41 +14,54 @@ vec7 = np.zeros((480, 640))
 vec8 = np.zeros((480, 640))
 vec9 = np.zeros((480, 640))
 vec10 = np.zeros((480, 640))
+# Vector für das Durchschnittsbild
 average = np.zeros((480, 640))
 
+# String für die einzelnen Dateinamen
 whitepic = ["white1", "white2", "white3", "white4", "white5", "white6", "white7", "white8", "white9", "white10"]
+# Vektornamen in einem Vektor
 vector = [vec1, vec2, vec3, vec4, vec5, vec6, vec7, vec8, vec9, vec10]
 
+# for-Schleife um auf alle 10 Dateien zuzugreifen
 for x in range(0, 10):
-    whitepic[x] = cv2.imread('data/' + whitepic[x] + '.png')
-    whitepic[x] = cv2.cvtColor(whitepic[x], cv2.COLOR_BGR2GRAY)
+    # Weißbilder einlesen
+    whitepic[x] = cv2.imread('data/' + whitepic[x] + '.png', cv2.IMREAD_GRAYSCALE)
 
-    print("Datei: " + str(x + 1) + ".png erfolgreich")
+    # for-Schleife um auf alle 307200 Pixel zuzugreifen
     for y in range(0, 480):
         for z in range(0, 640):
-            b = whitepic[x][y, z]
-            vector[x][y, z] = b
-            anzahl += 1
-    print("-----------------------------------")
+            # Vektoren der Dateien füllen mit den Bildpixelvalues
+            vector[x][y, z] = whitepic[x][y, z]
 
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for y in range(0, 480):
     for z in range(0, 640):
+        # Mean auf 0 zurück zu setzen
         mean = 0
+        # for-Schleife um auf alle 10 Dateien zuzugreifen
         for file in range(0, 10):
+            # Pixel aus allen Dateien welche auf der selben Stelle liegen addieren
             mean += vector[file][y, z]
-        mean = float(mean / 10)
-        average[y, z] = mean
+        # Pixel nun durch die Anzahl der Dateien teilen um den Mittelwert des Pixels zu erlangen
+        average[y, z] = float(mean / 10)
 
+# Datei einlesen um sie zu überschreiben
 image = cv2.imread("data/whiteaverage.png", 0)
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for x in range(0, 480):
     for y in range(0, 640):
+        # Bild erstellen mit den Mittelwert der Pixels
         image[x, y] = average[x, y]
 
+# Bild exportieren
 cv2.imwrite("data/whiteaverage.png", image)
+# Bild anzeigen
 cv2.imshow('image', image)
 
+# Bild kontrastmaximiert darstellen
 image_contrast = cv2.equalizeHist(image)
 
+# kontrastmaximiertes Bild exportieren und anzeigen
 cv2.imwrite("data/contrastwhiteaverage1.png", image_contrast)
 cv2.imshow('image_contrast', image_contrast)
 cv2.waitKey(0)
--- Versuch2/task3.3.py
+++ Versuch2/task3.3.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import math
 
 # ------------------------------ Aufgabe2.2 ------------------------------ #
 
