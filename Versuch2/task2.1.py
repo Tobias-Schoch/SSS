a53f13df4a44a6352161f8b52387457e687bda6a
Kennylele <tobias@schochwebdesign.de>
2019-04-18 00:01:21 +0200

    adding comments

--- Versuch2/task2.1.py
+++ Versuch2/task2.1.py
@@ -3,10 +3,7 @@ import cv2
 
 # ------------------------------ Aufgabe2.1 ------------------------------ #
 
-vec = np.zeros((10, 3079000))
-anzahl = 0
-mean = 0
-
+# Vectoren für die 10 Dunkelbildern
 vec1 = np.zeros((480, 640))
 vec2 = np.zeros((480, 640))
 vec3 = np.zeros((480, 640))
@@ -17,40 +14,54 @@ vec7 = np.zeros((480, 640))
 vec8 = np.zeros((480, 640))
 vec9 = np.zeros((480, 640))
 vec10 = np.zeros((480, 640))
-
+# Vector für das Durchschnittsbild
 average = np.zeros((480, 640))
 
+# String für die einzelnen Dateinamen
 blackpic = ["black1", "black2", "black3", "black4", "black5", "black6", "black7", "black8", "black9", "black10"]
+# Vektornamen in einem Vektor
 vector = [vec1, vec2, vec3, vec4, vec5, vec6, vec7, vec8, vec9, vec10]
 
+# for-Schleife um auf alle 10 Dateien zuzugreifen
 for x in range(0, 10):
-    blackpic[x] = cv2.imread('data/' + blackpic[x] + '.png')
-    blackpic[x] = cv2.cvtColor(blackpic[x], cv2.COLOR_BGR2GRAY)
+    # Dunkelbilder einlesen
+    blackpic[x] = cv2.imread('data/' + blackpic[x] + '.png', cv2.IMREAD_GRAYSCALE)
 
-    print("Datei: " + str(x + 1) + ".png erfolgreich")
+    # for-Schleife um auf alle 307200 Pixel zuzugreifen
     for y in range(0, 480):
         for z in range(0, 640):
+            # Vektoren der Dateien füllen mit den Bildpixelvalues
             vector[x][y, z] = blackpic[x][y, z]
-    print("-----------------------------------")
 
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for y in range(0, 480):
     for z in range(0, 640):
+        # Mean auf 0 zurück zu setzen
         mean = 0
+        # for-Schleife um auf alle 10 Dateien zuzugreifen
         for file in range(0, 10):
+            # Pixel aus allen Dateien welche auf der selben Stelle liegen addieren
             mean += vector[file][y, z]
-        mean = float(mean / 10)
-        average[y, z] = mean
+        # Pixel nun durch die Anzahl der Dateien teilen um den Mittelwert des Pixels zu erlangen
+        average[y, z] = float(mean / 10)
 
+# Datei einlesen um sie zu überschreiben
 image = cv2.imread("data/blackaverage.png", 0)
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for x in range(0, 480):
     for y in range(0, 640):
+        # Bild erstellen mit den Mittelwert der Pixels
         image[x, y] = average[x, y]
 
+# Bild exportieren
 cv2.imwrite("data/blackaverage.png", image)
+# Bild anzeigen
 cv2.imshow('image', image)
 
+# Bild kontrastmaximiert darstellen
 image_contrast = cv2.equalizeHist(image)
 
+# kontrastmaximiertes Bild exportieren und anzeigen
 cv2.imwrite("data/contrastblackaverage.png", image_contrast)
 cv2.imshow('image_contrast', image_contrast)
 cv2.waitKey(0)
--- Versuch2/task2.2.py
+++ Versuch2/task2.2.py
@@ -3,23 +3,33 @@ import cv2
 
 # ------------------------------ Aufgabe2.2 ------------------------------ #
 
+# Vector für das Korrekturbild
 korrigiertes_bild = np.zeros((480, 640))
 
-grey = cv2.imread('data/Versuch1a.png')
-image = cv2.cvtColor(grey, cv2.COLOR_BGR2GRAY)
+# Bild des Grauwertkeils einlesen und in Schwarz-Weiß umwandeln
+grey = cv2.imread('data/Versuch1a.png', cv2.IMREAD_GRAYSCALE)
+# Bild des Pixelweisen Mittelwerts einlesen
 blackavg = cv2.imread('data/blackaverage.png')
 
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for y in range(0, 480):
     for z in range(0, 640):
+        # Auf Grauwert des Pixels zugreifen des Grauwertkeils
         r1, g1, b1 = grey[y, z]
+        # Auf Grauwert des Pixels zugreifen des Dunkelbildes
         r2, g2, b2 = blackavg[y, z]
+        # Grauwertkeil - Dunkelbild
         korrigiertes_bild[y, z] = int(r1) - int(r2)
 
+# Datei einlesen um sie zu überschreiben
 image = cv2.imread("data/korrigiertes_bild.png")
+# for-Schleife um auf alle 307200 Pixel zuzugreifen
 for x in range(0, 480):
     for y in range(0, 640):
+        # Bild erstellen mit den Mittelwert der Pixels
         image[x, y] = korrigiertes_bild[x, y]
 
+# Bild exportieren und anzeigen
 cv2.imwrite("data/korrigiertes_bild.png", image)
 cv2.imshow('image', image)
 cv2.waitKey(0)
