e612c4eef445debdea37a30f08a10f78f4414261
Kennylele <tobias@schochwebdesign.de>
2019-05-08 23:06:30 +0200

    Fouriertransformation and Amplitude

--- Versuch3/task1.2.py
+++ Versuch3/task1.2.py
@@ -3,12 +3,12 @@ import numpy as np
 
 x, y = np.loadtxt('data/Mundharmonika/eins.csv', delimiter=',', unpack=True)
 data = np.loadtxt('data/Mundharmonika/eins.csv', delimiter=',', unpack=True)
-data_fft = np.fft.fft(data[:,1])
 
-plt.plot(x, y)
-plt.figure()
+# Darstellung der Logarithmierung von der avg Spannung und dem Abstand
+plt.plot(x, y, 'b')
 plt.ylabel('Spannung in V')
 plt.xlabel('Zeit t')
+plt.grid(True)
 plt.show()
 
 # Grundperiode:     1,586 ms
@@ -17,27 +17,4 @@ plt.show()
 # Abtastintervall:  4 μs
 # Signallänge:      2500
 # Abtastfrequenz:   4 MH
-period = 1.586
-frequence = 630.52
-singaltime = 10
-keyintervall = 4
-signallength = 2499
-keyfrequence = 4
-step = 1
-result = np.zeros((step,signallength+1))
-amplitude = signallength * keyfrequence
-i = 0
 
-for x in range(1,signallength,1):
-    i = i + 1
-    print(i)
-    print(y[x], "\n")
-
-for x in range(step,signallength,step):
-    result[0, x-1] = x / amplitude
-
-#plt.plot(result, y)
-#plt.figure()
-#plt.ylabel('Spannung in V')
-#plt.xlabel('Zeit t')
-#plt.show()
