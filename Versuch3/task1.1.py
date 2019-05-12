5be85c99ca8af21b34bb71b92512dbd1b9ccefc9
Kennylele <tobias@schochwebdesign.de>
2019-05-12 13:40:45 +0200

    Finished

--- Versuch3/task1.1.py
+++ Versuch3/task1.1.py
@@ -6,8 +6,9 @@ scope = TekTDS2000()
 scope.saveCsv(filename='versuch3/kleinerLautsprecher/100.csv', ch=1)
 scope.saveCsv(filename='100_2.csv', ch=2)
 
+# Einlesen der Frequenz und der Periode
 frequency = scope.getFreq(1)
 period = scope.getPeriod(1)
-
+# Ausgeben der Frequenz und der Periode
 print("Frequenz", frequency)
 print("Periode", period)
