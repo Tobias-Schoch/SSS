76ce014c951b956bf2704076ac827c55fa46a917
Kennylele <tobias@schochwebdesign.de>
2019-05-12 02:10:27 +0200

    LaTeX Versuch 1 finished

--- Versuch3/task1.1.py
+++ Versuch3/task1.1.py
@@ -5,3 +5,9 @@ scope = TekTDS2000()
 # Einlesen vom Channel 1 und vom Channel 2
 scope.saveCsv(filename='versuch3/kleinerLautsprecher/100.csv', ch=1)
 scope.saveCsv(filename='100_2.csv', ch=2)
+
+frequency = scope.getFreq(1)
+period = scope.getPeriod(1)
+
+print("Frequenz", frequency)
+print("Periode", period)
