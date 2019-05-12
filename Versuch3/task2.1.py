556fa8459bf2db505dcf302685ab5269fb2d1634
Kennylele <tobias@schochwebdesign.de>
2019-05-12 04:13:37 +0200

    LaTeX Task 2 Auswertung

--- Versuch3/task2.1.py
+++ Versuch3/task2.1.py
@@ -3,12 +3,12 @@ import numpy as np
 
 # Array um die einzelnen .csv Dateien einzulesen des kleinen Lautsprechers
 klein = ["klein100", "klein200", "klein300", "klein400", "klein500", "klein700", "klein850", "klein1000",
-         "klein1200", "klein1500", "klein1700", "klein2000", "klein3000", "klein4000", "klein5000", "klein6000",
-         "klein10000"]
+         "klein1200", "klein1500", "klein1700", "klein2000", "klein3000", "klein4000", "klein5000",
+         "klein6000", "klein10000"]
 # Array um die einzelnen .csv Dateien einzulesen des großen Lautsprechers
 gross = ["gross100", "gross200", "gross300", "gross400", "gross500", "gross700", "gross850", "gross1000",
-         "gross1200", "gross1500", "gross1700", "gross2000", "gross3000", "gross4000", "gross5000", "gross6000",
-         "gross10000"]
+         "gross1200", "gross1500", "gross1700", "gross2000", "gross3000", "gross4000", "gross5000",
+         "gross6000", "gross10000"]
 
 # for-Schleife für die 17 unterschiedlichen .csv Dateien
 for a in range(0, 17):
@@ -40,10 +40,12 @@ for a in range(0, 17):
     # Die ersten beiden Zeiten für den großen Lautsprecher werden in time1 geschrieben
     time2 = u[:2, ]
 
-    # Die zwei Zeiten des kleinen Lautsprechers werden subtrahiert und multipliziert für eine mikrosekunden Darstellung
+    # Die zwei Zeiten des kleinen Lautsprechers werden subtrahiert und multipliziert
+    # für eine mikrosekunden Darstellung
     # Zudem wird die berechnete Dauer gerundet
     timing1 = round((time1[1] - time1[0]) * 100000, 5)
-    # Die zwei Zeiten des großen Lautsprechers werden subtrahiert und multipliziert für eine mikrosekunden Darstellung
+    # Die zwei Zeiten des großen Lautsprechers werden subtrahiert und multipliziert
+    # für eine mikrosekunden Darstellung
     # Zudem wird die berechnete Dauer gerundet
     timing2 = round((time2[1] - time2[0]) * 100000, 5)
 
--- Versuch3/task2.2.py
+++ Versuch3/task2.2.py
@@ -3,17 +3,17 @@ import numpy as np
 
 # Array um die einzelnen .csv Dateien einzulesen des kleinen Lautsprechers
 klein = ["klein100", "klein200", "klein300", "klein400", "klein500", "klein700", "klein850", "klein1000",
-         "klein1200", "klein1500", "klein1700", "klein2000", "klein3000", "klein4000", "klein5000", "klein6000",
-         "klein10000"]
+         "klein1200", "klein1500", "klein1700", "klein2000", "klein3000", "klein4000",
+         "klein5000", "klein6000", "klein10000"]
 # Array um die einzelnen .csv Dateien einzulesen des großen Lautsprechers
 gross = ["gross100", "gross200", "gross300", "gross400", "gross500", "gross700", "gross850", "gross1000",
-         "gross1200", "gross1500", "gross1700", "gross2000", "gross3000", "gross4000", "gross5000", "gross6000",
-         "gross10000"]
+         "gross1200", "gross1500", "gross1700", "gross2000", "gross3000", "gross4000",
+         "gross5000", "gross6000", "gross10000"]
 
 # Die für jeweils den großen und den kleinen Lautsprecher gemessenen Zeiten
 zeit = [100, 200, 300, 400, 500, 700, 850, 1000, 1200, 1500, 1700, 2000, 3000, 4000, 5000, 6000, 10000]
 # Die für den kleinen Lautpsrecher von Hand berechneten Phasenverschiebungen in µs
-kleinphase = [228, 295, 113.2, 105.2, 70.6, 64.8, 34, 4.2, 1.8, 12.2, 12.1, 14.7, 8.88, 11.60, 5.92, 6.92, 3.67]
+kleinphase = [228, 187, 113.2, 105.2, 70.6, 64.8, 34, 4.2, 1.8, 12.2, 12.1, 14.7, 8.88, 11.60, 5.92, 6.92, 3.67]
 # Die für den großen Lautpsrecher von Hand berechneten Phasenverschiebungen in µs
 grossphase = [505, 82, 1, 10, 17, 12.4, 15.2, 14.8, 15.6, 12.8, 13.2, 14.4, 19.7, 12.52, 5.32, 3.32, 4.74]
 # Die einzelnen Abtastintervalle für die einzelnen Frequenzen des großen Lautsprechers
