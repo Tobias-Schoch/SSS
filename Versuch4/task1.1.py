ac692abe3f5fda2e3a7a1f5cdd61576857c439dd
Kennylele <tobias@schochwebdesign.de>
2019-05-21 12:28:20 +0200

    Trigger mic input

---
+++ Versuch4/task1.1.py
@@ -0,0 +1,30 @@
+import pyaudio
+import numpy
+import matplotlib.pyplot as plt
+
+FORMAT = pyaudio.paInt16
+SAMPLEFREQ = 44100
+FRAMESIZE = 1024
+NOFFRAMES = 220
+p = pyaudio.PyAudio()
+print('running')
+
+stream = p.open(format=FORMAT,
+                channels=1,
+                rate=SAMPLEFREQ,
+                input=True,
+                frames_per_buffer=FRAMESIZE)
+data = stream.read(NOFFRAMES*FRAMESIZE)
+decoded = numpy.fromstring(data, 'Int16');
+numpy.save('aufgabe4/test.npy', decoded)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+print('done')
+plt.plot(decoded)
+plt.xlabel('Zeit t')
+plt.ylabel('Frequenz f')
+plt.savefig('aufgabe4/test.png')
+plt.show()
+
