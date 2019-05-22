8a606e67b3aa4fdc0cedc9a5e5ab2063f4790474
Kennylele <tobias@schochwebdesign.de>
2019-05-22 15:18:46 +0200

    Working on Windowing method

--- Versuch4/task1.3.py
+++ Versuch4/task1.3.py
@@ -3,31 +3,13 @@ import numpy as np
 
 # Einlesen der .csv Datei
 data = np.load('data/test.npy')
-freq = np.zeros(225280)
+window = np.zeros((881, 513))
+z = 511
 
-# Darstellung des Amplitudenspektrums
-plt.plot(data)
-plt.grid()
-plt.xlabel('Frequency in Hz')
-plt.ylabel('Amplitude in V')
-plt.savefig('data/img/testamp.png')
-plt.show()
+for y in range(0, 878):
+    z = z - 256
+    for x in range(0, 512):
+        z = z + 1
+        window[y, x] = data[z]
 
-# Der zweite Wert wird absolut minus den ersten absoluten wert gerechnet um später den Wert
-difference = 2 / 225280
-# Die zweite Spalte der .csv Datei wird Fouriertransformiert
-fourier = np.fft.fft(data[:225280])
-# Die Fouriertransformierte Frequenz wird absolutiert, so dass kein negativer Wert mehr vorzufinden ist
-spektrum = np.abs(fourier)
-# Formel um die Anzahl der Schwingungen in die Freuquenz umzurechnen - f = n / (M * Δt)
-for x in range(0, 225280, 1):
-    freq[x] = (x / (difference * 225280))
 
-# Darstellung des Amplitudenspektrums
-plt.plot(freq, spektrum)
-plt.grid()
-plt.xlabel('Frequency in Hz')
-plt.ylabel('Amplitude in V')
-plt.savefig('data/img/testspektrum.png')
-plt.xlim(0, 40000)
-plt.show()
