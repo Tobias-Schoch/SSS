c6f03de529b5f3897147f85076f782de0e43673d
Kennylele <tobias@schochwebdesign.de>
2019-05-22 14:31:00 +0200

    Added data

--- Versuch4/task1.3.py
+++ Versuch4/task1.3.py
@@ -10,7 +10,7 @@ plt.plot(data)
 plt.grid()
 plt.xlabel('Frequency in Hz')
 plt.ylabel('Amplitude in V')
-plt.savefig('data/img/hoch1.png')
+plt.savefig('data/img/testamp.png')
 plt.show()
 
 # Der zweite Wert wird absolut minus den ersten absoluten wert gerechnet um später den Wert
@@ -21,13 +21,13 @@ fourier = np.fft.fft(data[:225280])
 spektrum = np.abs(fourier)
 # Formel um die Anzahl der Schwingungen in die Freuquenz umzurechnen - f = n / (M * Δt)
 for x in range(0, 225280, 1):
-    freq[x] = x / (difference * 225280)
+    freq[x] = (x / (difference * 225280))
 
 # Darstellung des Amplitudenspektrums
 plt.plot(freq, spektrum)
 plt.grid()
 plt.xlabel('Frequency in Hz')
 plt.ylabel('Amplitude in V')
-plt.savefig('data/img/hoch1.png')
+plt.savefig('data/img/testspektrum.png')
 plt.xlim(0, 40000)
 plt.show()
